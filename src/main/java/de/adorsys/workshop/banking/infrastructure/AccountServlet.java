faa5b8bd3161f48d2810fcb0ababf1ba2b6f36ec
Roman Ataman <roman.ataman@adorsys.de>
2019-04-04 15:27:41 +0200

    Add GET account/:iban

--- src/main/java/de/adorsys/workshop/banking/infrastructure/AccountServlet.java
+++ src/main/java/de/adorsys/workshop/banking/infrastructure/AccountServlet.java
@@ -1,4 +1,40 @@
 package de.adorsys.workshop.banking.infrastructure;
 
-public class AccountServlet {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.adorsys.workshop.banking.domain.Account;
+import de.adorsys.workshop.banking.domain.Money;
+import org.graalvm.compiler.lir.CompositeValue;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountServlet extends HttpServlet {
+
+  private Map<String, Account> accounts = new HashMap<>();
+  private ObjectMapper om;
+
+  public AccountServlet(ObjectMapper om) {
+    this.om = om;
+    accounts.put("DE1234567890", new Account("DE1234567890", "1234567", new Money("EUR", 1000), "Roman"));
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+    String pathInfo = req.getPathInfo();
+    resp.setHeader("content-type", "application/json");
+    String body;
+    if (pathInfo == null || pathInfo.equals("/")) {
+      body = om.writeValueAsString(accounts.values());
+    } else {
+      String iban = pathInfo.replaceAll("\\/", "");
+      body = om.writeValueAsString(accounts.get(iban));
+    }
+    resp.getWriter().write(body);
+  }
 }
--- src/main/java/de/adorsys/workshop/banking/infrastructure/WebApplication.java
+++ src/main/java/de/adorsys/workshop/banking/infrastructure/WebApplication.java
@@ -1,14 +1,22 @@
 package de.adorsys.workshop.banking.infrastructure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class WebApplication {
   public static void main(String[] args) throws Exception {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
     Server server = new Server(8080);
 
     ServletHandler servletHandler = new ServletHandler();
-    servletHandler.addServletWithMapping(HelloWorldServlet.class, "/");
+    servletHandler.addServletWithMapping(
+      new ServletHolder(new AccountServlet(objectMapper)),
+      "/accounts/*"
+    );
     server.setHandler(servletHandler);
 
     server.start();
